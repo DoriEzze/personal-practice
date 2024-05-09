2aa1d8036285a3db4d765c36cce02ff7a47ee0e1
Cami <camila.mduarte7@gmail.com>
2024-05-09 16:30:22 -0300

    Added Playlist reporitory and model

---
+++ src/main/java/com/accenture/practice/repositories/PlaylistRepository.java
@@ -0,0 +1,9 @@
+package com.accenture.practice.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.accenture.practice.models.Playlist;
+
+public interface PlaylistRepository extends CrudRepository<Playlist, Long>{
+
+}

595e52ac7b01478369745ace2ee0a56619ffcb91
JordanCalle <jordan.calle97@gmail.com>
2024-05-08 23:33:27 -0300

    Se añadió el modelo y repositorio PlaylistTrack.

---
+++ src/main/java/com/accenture/practice/models/PlaylistTrack.java
@@ -0,0 +1,20 @@
+package com.accenture.practice.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "PlaylistTrack")
+@Data
+public class PlaylistTrack {
+
+    @Id
+    @Column(name = "PlaylistId")
+    private Long playlistId;
+
+    @Column(name = "TrackId")
+    private Long trackId;
+}

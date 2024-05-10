72b8b86d2390c0248f09d03007ce95d940a0309a
luli25 <lucilaoliver8@gmail.com>
2024-05-10 12:11:38 -0300

    Added method to convert blob to a byte array

--- src/main/java/com/accenture/practice/models/Album.java
+++ src/main/java/com/accenture/practice/models/Album.java
@@ -1,10 +1,13 @@
 package com.accenture.practice.models;
 
+import com.accenture.practice.utils.BlobToByteArray;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+
+import java.sql.SQLException;
 
 @Entity
 @Table(name = "Album")
@@ -20,4 +23,12 @@ public class Album {
 
     @Column(name = "ArtistId")
     private Long artistId;
+
+    @Column(name = "Column1")
+    private byte[] column1;
+
+    public void setColumn1(byte[] bytes) throws SQLException {
+        bytes = BlobToByteArray.convertToByteArray();
+        this.column1 = bytes;
+    }
 }

dff5bf729278d58f4bfeef0c9cfd4f5221a09d40
luli25 <lucilaoliver8@gmail.com>
2024-05-10 13:29:03 -0300

    Deleted conversion method

--- src/main/java/com/accenture/practice/models/Album.java
+++ src/main/java/com/accenture/practice/models/Album.java
@@ -1,13 +1,10 @@
 package com.accenture.practice.models;
 
-import com.accenture.practice.utils.BlobToByteArray;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
-
-import java.sql.SQLException;
 
 @Entity
 @Table(name = "Album")
@@ -26,9 +23,4 @@ public class Album {
 
     @Column(name = "Column1")
     private byte[] column1;
-
-    public void setColumn1(byte[] bytes) throws SQLException {
-        bytes = BlobToByteArray.convertToByteArray();
-        this.column1 = bytes;
-    }
 }

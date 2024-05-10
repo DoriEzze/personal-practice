ccf175fbed4a90dad84a1a6c1f28b5f7f2ecd12d
JordanCalle <jordan.calle97@gmail.com>
2024-05-10 14:30:05 -0300

    Corrección del Controller para que sea igual que la branch develop-spring-data

--- src/main/java/com/accenture/practice/controller/SimpleController.java
+++ src/main/java/com/accenture/practice/controller/SimpleController.java
@@ -1,18 +1,12 @@
 package com.accenture.practice.controller;
 
-import com.accenture.practice.models.Playlist;
 import com.accenture.practice.models.Album;
 import com.accenture.practice.repositories.AlbumRepository;
-import com.accenture.practice.repositories.PlaylistRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-
 
 @Controller
 public class SimpleController {
@@ -22,23 +16,10 @@ public class SimpleController {
     @Autowired
     AlbumRepository albumRepository;
 
-    @Autowired
-    PlaylistRepository playlistRepository;
-
-    // @GetMapping("/")
-    // public String homePage(Model model) {
-    //     Album album = albumRepository.findById(1L).get();
-    //     model.addAttribute("appName", album.getTitle());
-    //     return "home";
-    // }
-
-    @GetMapping("/Playlist/{id}")
-    public String homePage1(@PathVariable Long id, Model model) {
-        Playlist playlist = playlistRepository.findById(id).get();  //findById(1L).get();
-        model.addAttribute("appName", playlist.getName());
+    @GetMapping("/")
+    public String homePage(Model model) {
+        Album album = albumRepository.findById(1L).get();
+        model.addAttribute("appName", album.getTitle());
         return "home";
     }
-
-   
-    
 }

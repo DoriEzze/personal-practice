713674ec0a9477edd09a118489132d73efe36d19
Gaston Areco <gaston.areco@teamcubation.com>
2024-07-02 16:10:36 -0300

    Spring JPA practice with pagination and sorting

---
+++ src/main/java/org/accenture/repository/TutorialRepository.java
@@ -0,0 +1,18 @@
+package org.accenture.repository;
+
+import jakarta.transaction.Transactional;
+import org.accenture.model.Tutorial;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Date;
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+
+}
